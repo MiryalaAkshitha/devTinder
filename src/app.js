eca1bfa199945c6293a30263a13c6942dd7c0d0d
akshitha <akshita@vider.in>
2025-04-25 10:16:43 +0530

    route handlers and next function

--- src/app.js
+++ src/app.js
@@ -2,43 +2,47 @@ const express = require("express");
 const app = express()
 
 
+// app.use("/user",(req,res)=>{
+//     console.log('hiii')
+    //1 .because we are not sending any request it will go into loop and console will be printed but response will not be shown
+// })
 
 
-app.get("/user",(req,res)=>{
-    // http://localhost:7777/user?userId=702
-    console.log(req.query)
-    res.send('you are fecthing get of user')
-})
+//2. response 1 will be printed as after send code will not be executed
+// app.use("/user",(req,res,next)=>{
+// res.send("response 1")
+// },(req,res,next)=>{
+// res.send("response 2")
+// })
+//3. response 1 will be printed as after send code will not be executed, same but will get error in console 
 
-app.get("/test/:userId/:name",(req,res)=>{
-    // http://localhost:7777/701
-    console.log(req.params)
-    res.send('you are fecthing get of user')
-})
+// app.use("/user",(req,res,next)=>{
+//     res.send("response 1")
+//     next()
+//     },(req,res,next)=>{
+//     res.send("response 2")
+//     })
+//3. hello will be printed and response 2 will be printed as we have next
 
+    // app.use("/user",(req,res,next)=>{
+    //     console.log("hello")
+    //     next()
+    //     },(req,res,next)=>{
+    //     res.send("response 2")
+    //     })
+
+//this will directly print hello 2 and send response 2 
+app.use("/user",(req,res,next)=>{
+    next()
+console.log("hello")
+        },(req,res,next)=>{
+            console.log("hello 2")
+
+        res.send("response 2")
+        })
 
 
 app.listen(7777,()=>{
     "server is running on 7777"
 })
 
-// app.get("/a?bc",(req,res) =>{
-//     res.send('matched route')
-// })
-
-// app.get("/ab+c	", (req, res) => {
-//     res.send("matched /abc or /ac");
-// });
-
-app.listen(7777, () => {
-    console.log("server is running on 7777");
-});
-
-
-// normal node server creation 
-// const http= require("http")
-// const server = http.createServer((req,res)=>{
-//     res.end("holllaa")
-// })
-
-// server.listen(8888) 
