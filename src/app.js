df5960cd2920cf8ed6e992cb354a0b13303973e2
akshitha <akshita@vider.in>
2025-04-25 09:05:07 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const app = express()
-app.use("/test",(req,res)=>{
-    res.send("heeelllllo from test")
+
+
+
+
+app.get("/user",(req,res)=>{
+    // http://localhost:7777/user?userId=702
+    console.log(req.query)
+    res.send('you are fecthing get of user')
 })
-app.use((req,res)=>{
-    res.send("hello fkrom dashbnnnoard")
+
+app.get("/test/:userId/:name",(req,res)=>{
+    // http://localhost:7777/701
+    console.log(req.params)
+    res.send('you are fecthing get of user')
 })
+
+
+
 app.listen(7777,()=>{
     "server is running on 7777"
 })
+
+// app.get("/a?bc",(req,res) =>{
+//     res.send('matched route')
+// })
+
+// app.get("/ab+c	", (req, res) => {
+//     res.send("matched /abc or /ac");
+// });
+
+app.listen(7777, () => {
+    console.log("server is running on 7777");
+});
+
 
 // normal node server creation 
 // const http= require("http")
